7dbbcdead3aead11799bc12e860eda2404142390
GuilhermeFerrarezi <99054954+GuilhermeFerrarezi@users.noreply.github.com>
2023-02-07 13:56:12 -0300

    Update classe_aluno.java

--- aula1/classe_aluno.java
+++ aula1/classe_aluno.java
@@ -1,5 +1,7 @@
 package aula1;
 
+import java.util.Arrays;
+
 public class Aluno {
 
 	String nome;
@@ -34,27 +36,12 @@ public class Aluno {
 	}
 	
 	public void Media_aluno() {
-		double maior = 0;
-		double maior2 = 0;
-		if (this.nota_P1 >= this.nota_P2 && this.nota_P1 >= this.nota_P3) {
-			maior = this.nota_P1;
-		}
-		if (this.nota_P2 >= this.nota_P3 && this.nota_P2 >= this.nota_P1) {
-			maior = this.nota_P2;
-		}
-		if (this.nota_P3 >= this.nota_P1 && this.nota_P3 >= this.nota_P2) {
-			maior = this.nota_P3;
-		}
-		if ((this.nota_P1 >= this.nota_P2 || this.nota_P1 >= this.nota_P3) && maior != this.nota_P1) {
-			maior2 = this.nota_P1;
-		}
-		if ((this.nota_P2 >= this.nota_P1 || this.nota_P2 >= this.nota_P3) && maior != this.nota_P2) {
-			maior2 = this.nota_P2;
-		}
-		if ((this.nota_P3 >= this.nota_P1 || this.nota_P3 >= this.nota_P2) && maior != this.nota_P3) {
-			maior2 = this.nota_P3;
-		}
-		this.media = (maior + maior2)/2;
+		double[] notas = new double[3];
+		notas[0] = this.nota_P1;
+		notas[1] = this.nota_P2;
+		notas[2] = this.nota_P3;
+		Arrays.sort(notas);
+		this.media = (notas[1] + notas[2]) / 2;
 	}
 	
 	public void Resultado() {
@@ -68,5 +55,4 @@ public class Aluno {
 	public void Imprime_media() {
 		System.out.println(this.media + "\n");
 	}
-	
 }
