2c73616bf26036c7d33bcd0a652d99586809c87c
GuilhermeFerrarezi <99054954+GuilhermeFerrarezi@users.noreply.github.com>
2023-02-08 15:01:53 -0300

    Update Aluno.java

--- aula1/codigo_melhorado/Aluno.java
+++ aula1/codigo_melhorado/Aluno.java
@@ -12,8 +12,9 @@ public class Aluno {
 	double nota_P2;
 	double nota_P3;
 	double media;
-	
-	public Aluno(String nome, int codmat, String cpf, char sexo, double nota_P1,double nota_P2, double nota_P3) {
+	static int qtd_notas = 3;
+
+	public Aluno(String nome, int codmat, String cpf, char sexo, double nota_P1, double nota_P2, double nota_P3) {
 		this.nome = nome;
 		this.codmat = codmat;
 		this.cpf = cpf;
@@ -23,27 +24,34 @@ public class Aluno {
 		this.nota_P3 = nota_P3;
 	}
 	
+	public Aluno(String nome, int codmat, String cpf, char sexo) {
+		this.nome = nome;
+		this.codmat = codmat;
+		this.cpf = cpf;
+		this.sexo = sexo;
+	}
+
 	public void Imprime_aluno() {
 		System.out.println(this.nome + ' ' + this.codmat + ' ' + this.cpf + ' ' + this.sexo + ' ' + this.nota_P1 + ' ' + this.nota_P2 + ' ' + this.nota_P3 + "\n");
 	}
-	
+
 	public void Imprime_sexo() {
 		if (this.sexo == 'M') {
-			System.out.println(this.nome + ' ' + "Masculino\n");
+			System.out.println(this.nome + " - " + "Masculino\n");
 		}else if (this.sexo == 'F') {
-			System.out.println(this.nome + ' ' + "Feminino\n");
+			System.out.println(this.nome + " - "  + "Feminino\n");
 		}
 	}
-	
+
 	public void Media_aluno() {
-		double[] notas = new double[3];
+		double[] notas = new double[qtd_notas];
 		notas[0] = this.nota_P1;
 		notas[1] = this.nota_P2;
 		notas[2] = this.nota_P3;
 		Arrays.sort(notas);
 		this.media = (notas[1] + notas[2]) / 2;
 	}
-	
+
 	public void Resultado() {
 		if (this.media >= 6) {
 			System.out.println("Aprovado\n");
@@ -51,7 +59,7 @@ public class Aluno {
 			System.out.println("Reprovado\n");
 		}
 	}
-	
+
 	public void Imprime_media() {
 		System.out.println(this.media + "\n");
 	}
