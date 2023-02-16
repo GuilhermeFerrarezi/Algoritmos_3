f2e2ae8fd5a578123253775a41cc1c6bae823aec
GuilhermeFerrarezi <99054954+GuilhermeFerrarezi@users.noreply.github.com>
2023-02-16 10:12:06 -0300

    Create 1007 - Diferença.java

---
+++ estudos_por_fora/beecrowd/iniciante/1007 - Diferença.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        
+        Scanner entrada = new Scanner(System.in);
+        int a = entrada.nextInt();
+        int b = entrada.nextInt();
+        int c = entrada.nextInt();
+        int d = entrada.nextInt();
+        int diferenca = ((a * b) - (c * d));
+        entrada.close();
+        
+        System.out.println("DIFERENCA = " + diferenca);
+    }
+}
