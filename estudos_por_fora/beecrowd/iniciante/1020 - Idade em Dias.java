9be97fd674ab3cf695b40976758a55c6c1dcd270
GuilhermeFerrarezi <99054954+GuilhermeFerrarezi@users.noreply.github.com>
2023-02-16 10:24:43 -0300

    Create 1020 - Idade em Dias.java

---
+++ estudos_por_fora/beecrowd/iniciante/1020 - Idade em Dias.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        
+        Scanner entrada = new Scanner(System.in);
+        int dias = entrada.nextInt();
+        entrada.close();
+        int ano = dias / 365;
+        dias %= 365;
+        int mes = dias / 30;
+        dias %= 30;
+        
+        System.out.println(ano + " ano(s)");
+        System.out.println(mes + " mes(es)");
+        System.out.println(dias + " dia(s)");
+    }
+}
