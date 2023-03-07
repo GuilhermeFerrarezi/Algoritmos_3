ba9dd5a633e88a2246ccfad45980696a41cec92f
GuilhermeFerrarezi <99054954+GuilhermeFerrarezi@users.noreply.github.com>
2023-03-07 11:42:04 -0300

    Create Node.java

---
+++ Aula5 parte2/Node.java
@@ -0,0 +1,16 @@
+package teste2;
+
+public class Node {
+	int item;
+	Node next;
+	
+	public Node() {
+		this.item = 0;
+		this.next = null;
+	}
+	
+	public Node(int item) {
+		this.item = item;
+		this.next = null;
+	}
+}
