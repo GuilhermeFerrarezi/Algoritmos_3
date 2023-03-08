b4a2946daad09dc3803772071452eb6b1fd95916
GuilhermeFerrarezi <99054954+GuilhermeFerrarezi@users.noreply.github.com>
2023-03-08 14:00:18 -0300

    Create 1143 - Quadrado e ao Cubo.java

---
+++ estudos_por_fora/beecrowd/iniciante/1143 - Quadrado e ao Cubo.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        
+        Scanner entrada = new Scanner(System.in);
+        int qtd = entrada.nextInt();
+        entrada.close();
+        
+        for(int i = 1; i <= qtd; i++){
+            System.out.printf("%d %d %d\n", i, (int) Math.pow(i, 2), (int) Math.pow(i, 3));
+        }
+    }
+}
