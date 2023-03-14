cf943d7eaf0848c7504ed7ba5180a78d8a96b00b
GuilhermeFerrarezi <99054954+GuilhermeFerrarezi@users.noreply.github.com>
2023-03-14 14:45:35 -0300

    Update DList.java

--- aula6/DList.java
+++ aula6/DList.java
@@ -20,71 +20,116 @@ public class DList {
 	}
 	
 	public void imprime_First() {
-		System.out.println(this.head.item);
+		if(this.head == null){
+			System.out.println("Lista vazia");
+		}else {
+			System.out.println(this.head.item);
+		}
 	}
 	
 	public void imprime_Last() {
-		System.out.println(this.tail.item);
+		if(this.tail == null) {
+			System.out.println("Lista vazia");
+		}else {
+			System.out.println(this.tail.item);
+		}
 	}
 	
 	public void insereFim(int item) {
 		DListNode no_trab = new DListNode(item);
-		no_trab.prev = this.tail;
-		this.tail = no_trab;
+		if(this.size == 0) {
+			this.head = no_trab;
+			this.tail = no_trab;
+		}else {
+			no_trab.prev = this.tail;
+			this.tail.next = no_trab;
+			this.tail = no_trab;
+		}
 		this.size++;
 	}
 	
 	public void insereInicio(int item) {
 		DListNode no_trab = new DListNode(item);
-		no_trab.next = this.head;
-		this.head = no_trab;
+		if(this.size == 0) {
+			this.head = no_trab;
+			this.tail = no_trab;
+		}else {
+			no_trab.next = this.head;
+			this.head.prev = no_trab;
+			this.head = no_trab;
+		}
 		this.size++;
 	}
 	
 	public void imprimeLista() {
-		DListNode no_trab = this.head;
-		for(int i = 0; i < this.size; i++) {
-			System.out.println(no_trab.item);
-			no_trab = no_trab.next;
+		if(this.size == 0) {
+			System.out.println("Lista Vazia");
+		}else {
+			DListNode no_trab = this.head;
+			for(int i = 0; i < this.size; i++) {
+				System.out.println(no_trab.item);
+				no_trab = no_trab.next;
+			}
 		}
 	}
 	
 	public void imprimeLista2() {
-		DListNode no_trab = this.tail;
-		for(int i = this.size; i > 0; i--) {
-			System.out.println(no_trab.item);
-			no_trab = no_trab.prev;
+		if(this.size == 0) {
+			System.out.println("Lista Vazia");
+		}else {
+			DListNode no_trab = this.tail;
+			for(int i = this.size; i > 0; i--) {
+				System.out.println(no_trab.item);
+				no_trab = no_trab.prev;
+			}
 		}
 		
 	}
 	
 	public void deleteFirst() {
-		this.head = this.head.next;
-		this.head.prev = null;
-		this.size--;
+		if(this.size == 0) {
+			System.out.println("Lista Vazia");
+		}else {
+			this.head = this.head.next;
+			this.head.prev = null;
+			this.size--;
+		}
 	}
 	
 	public void deleteLast() {
-		this.tail = this.tail.prev;
-		this.tail.next = null;
-		this.size--;
+		if(this.size == 0) {
+			System.out.println("Lista Vazia");
+		}else {
+			this.tail = this.tail.prev;
+			this.tail.next = null;
+			this.size--;
+		}
 	}
 	
 	public double MediaLista() {
-		double media = 0;
-		DListNode no_trab = this.head;
-		for(int i = 0; i < this.size; i++) {
-			media += no_trab.item;
-			no_trab = no_trab.next;
+		if(this.size == 0) {
+			System.out.println("Lista Vazia");
+			return 0;
+		}else {
+			double media = 0;
+			DListNode no_trab = this.head;
+			for(int i = 0; i < this.size; i++) {
+				media += no_trab.item;
+				no_trab = no_trab.next;
+			}
+			return media/this.size;
 		}
-		return media/this.size;
 	}
 	
 	public void DobraValores() {
-		DListNode no_trab = this.head;
-		for(int i = 0; i < this.size; i++) {
-			no_trab.item *= 2;
-			no_trab = no_trab.next;
+		if(this.size == 0) {
+			System.out.println("Lista Vazia");
+		}else {
+			DListNode no_trab = this.head;
+			for(int i = 0; i < this.size; i++) {
+				no_trab.item *= 2;
+				no_trab = no_trab.next;
+			}
 		}
 	}
 	
@@ -97,22 +142,31 @@ public class DList {
 	}
 	
 	public DListNode[] GeraArray() {
-		DListNode[] no_trab = new DListNode[this.size];
-		DListNode no_trab2 = this.head;
-		for(int i = 0; i < this.size; i++) {
-			no_trab[i] = no_trab2;
-			no_trab2 = no_trab2.next;
+		if(this.size == 0) {
+			System.out.println("Lista Vazia");
+			return null;
+		}else {
+			DListNode[] no_trab = new DListNode[this.size];
+			DListNode no_trab2 = this.head;
+			for(int i = 0; i < this.size; i++) {
+				no_trab[i] = no_trab2;
+				no_trab2 = no_trab2.next;
+			}
+			return no_trab;
 		}
-		return no_trab;
 	}
 	
 	public boolean SearchLista(int valor) {
-		DListNode no_trab = this.head;
-		for(int i = 0; i < this.size; i++) {
-			if(no_trab.item == valor) {
-				return true;
+		if(this.size == 0) {
+			System.out.println("Lista Vazia");
+		}else {
+			DListNode no_trab = this.head;
+			for(int i = 0; i < this.size; i++) {
+				if(no_trab.item == valor) {
+					return true;
+				}
+				no_trab = no_trab.next;
 			}
-			no_trab = no_trab.next;
 		}
 		return false;
 	}
