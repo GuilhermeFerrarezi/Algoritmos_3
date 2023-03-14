4565e6752c809df82783b4a1fc21f54780da3768
GuilhermeFerrarezi <99054954+GuilhermeFerrarezi@users.noreply.github.com>
2023-03-14 11:21:31 -0300

    Create  1186 - Abaixo da Diagonal Secundária.java

---
+++ estudos_por_fora/beecrowd/iniciante/1186 - Abaixo da Diagonal Secundária.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        
+        Scanner entrada = new Scanner(System.in);
+        Double[][] m = new Double[12][12];
+        double sum = 0;
+        int limite = m.length - 1, qtd = 0;
+        char o = entrada.nextLine().toUpperCase().charAt(0);
+        
+        for(int i = 0; i < m.length; i++, limite--){
+            for(int j = 0; j < m[i].length; j++){
+                m[i][j] = entrada.nextDouble();
+                if(j > limite){
+                    sum += m[i][j];
+                    qtd++;
+                }
+            }
+        }
+        entrada.close();
+        if(o == 'S'){
+            System.out.printf("%.1f\n", sum);
+        }else if(o == 'M'){
+            System.out.printf("%.1f\n", sum / qtd);
+        }
+    }
+}
