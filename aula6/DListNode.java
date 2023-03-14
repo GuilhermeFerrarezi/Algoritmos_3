3ce1c6e6fc6a6354cd7eedfdb7729f65719a1c56
GuilhermeFerrarezi <99054954+GuilhermeFerrarezi@users.noreply.github.com>
2023-03-13 22:25:43 -0300

    Create DListNode.java

---
+++ aula6/DListNode.java
@@ -0,0 +1,14 @@
+package uscs;
+
+public class DListNode {
+
+	int item;
+	DListNode next;
+	DListNode prev;
+	
+	public DListNode(int item) {
+		this.item = item;
+		this.next = null;
+		this.prev = null;
+	}
+}
