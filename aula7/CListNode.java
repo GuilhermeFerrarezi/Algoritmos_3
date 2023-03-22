678d9d20713b6e4f4dbe03572aa64fee0e3e74ec
GuilhermeFerrarezi <99054954+GuilhermeFerrarezi@users.noreply.github.com>
2023-03-22 08:27:25 -0300

    Create CListNode.java

---
+++ aula7/CListNode.java
@@ -0,0 +1,21 @@
+package uscs2;
+
+public class CListNode {
+
+	int item;
+	CListNode next;
+	CListNode prev;
+	
+	public CListNode() {
+		this.item = 0;
+		this.next = null;
+		this.prev = null;
+	}
+	
+	public CListNode(int item) {
+		this.item = item;
+		this.next = null;
+		this.prev = null;
+	}
+	
+}
