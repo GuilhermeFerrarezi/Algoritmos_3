8931acafb074c8ef1d2394be49d43b2890e5487b
GuilhermeFerrarezi <99054954+GuilhermeFerrarezi@users.noreply.github.com>
2023-03-22 16:19:02 -0300

    Update 1929 - Triângulo.java

--- estudos_por_fora/beecrowd/iniciante/1929 - Triângulo.java
+++ estudos_por_fora/beecrowd/iniciante/1929 - Triângulo.java
@@ -7,17 +7,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
       
       Scanner entrada = new Scanner(System.in);
-    	int[] a = new int[4];
-		  for(int i = 0; i < a.length; i++) {
-			a[i] = entrada.nextInt();
-		  }
-		  entrada.close();
-		  Arrays.sort(a);
-		  if ((a[1] + a[0] > a[3] ) || (a[1] + a[0] > a[2]) ||
-		    	(a[1] + a[2] > a[3]) || (a[0] + a[2] > a[3])) {
-		    System.out.println('S');
-		  }else {
-				System.out.println('N');
-		  }
-    }
+      int[] a = new int[4];
+      for(int i = 0; i < a.length; i++) {
+      	a[i] = entrada.nextInt();
+      }
+      entrada.close();
+      Arrays.sort(a);
+      if ((a[1] + a[0] > a[3] ) || (a[1] + a[0] > a[2]) ||
+	 (a[1] + a[2] > a[3]) || (a[0] + a[2] > a[3])) {
+ 	System.out.println('S');
+      }else {
+	System.out.println('N');
+      }
+   }
 }
