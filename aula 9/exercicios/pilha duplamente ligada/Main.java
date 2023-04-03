3e618abeb286776d01fc01302878200857dc3b1c
GuilhermeFerrarezi <99054954+GuilhermeFerrarezi@users.noreply.github.com>
2023-04-03 15:36:15 -0300

    Create Main.java

---
+++ aula 9/exercicios/pilha duplamente ligada/Main.java
@@ -0,0 +1,20 @@
+package teste7;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		pilha_D x = new pilha_D();
+		x.push(1);
+		x.push(2);
+		x.push(3);
+		x.imprime_pilha();
+		x.pop();
+		x.imprime_pilha();
+		x.pop();
+		x.imprime_pilha();
+		x.pop();
+		x.imprime_pilha();
+		x.pop();
+		x.imprime_pilha();
+  }
+}
