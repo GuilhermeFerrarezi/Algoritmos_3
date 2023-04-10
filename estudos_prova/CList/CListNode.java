aaa601b37d208ce5a1dd011fc1d4874b6868228a
GuilhermeFerrarezi <99054954+GuilhermeFerrarezi@users.noreply.github.com>
2023-04-10 16:12:13 -0300

    Create CListNode.java

---
+++ estudos_prova/CList/CListNode.java
@@ -0,0 +1,20 @@
+package prova7;
+
+public class CListNode {
+	
+	int item;
+	CListNode next;
+	CListNode prev;
+	
+	public CListNode() {
+		this.item = 0;
+		this.next = null;
+		this.prev = null;
+	}
+	
+	public CListNode(int item) {
+		this.item = item;
+		this.next = null;
+		this.prev = null;
+	}
+}
