4d08d0f80018452ea7507416bb7856fa8e28c294
GuilhermeFerrarezi <99054954+GuilhermeFerrarezi@users.noreply.github.com>
2023-04-10 16:04:15 -0300

    Create DListNode.java

---
+++ estudos_prova/DList/DListNode.java
@@ -0,0 +1,20 @@
+package prova6;
+
+public class DListNode {
+	
+	int item;
+	DListNode next;
+	DListNode prev;
+	
+	public DListNode() {
+		this.item = 0;
+		this.next = null;
+		this.prev = null;
+	}
+	
+	public DListNode(int item) {
+		this.item = item;
+		this.next = null;
+		this.prev = null;
+	}
+}
